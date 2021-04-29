2851d22a101810184c0777884b379b47e6de3d2e
yixiang.ong@bookmyshow.com <yixiang.ong@bookmyshow.com>
2021-04-30 00:59:28 +0800

    updates

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+export type TsEslintPluginRules = Record<
+  keyof typeof import('@typescript-eslint/eslint-plugin/dist/configs/all.json')['rules'],
+  any
+>
+
+export type EslintRules = import('eslint/rules/index').ESLintRules
+export type TsEslintRules = EslintRules & TsEslintPluginRules
+
+export type TsEslintConfig = {
+  rules?: TsEslintRules
+}
